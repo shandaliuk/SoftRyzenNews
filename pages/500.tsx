5eeafb56fc3c8f3ee06ae105b0b2a770224fb300
Kyrylo Shandaliuk <shandaliuk3019@gmail.com>
2023-05-22 18:20:26 +0300

    Add 404 and 500 errors pages

---
+++ pages/500.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+import Container from '@/components/SharedComponents/Container';
+
+const ErrorPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <section className="pt-[46px] md:pt-[52px] xl:pt-[51px] mb-8 grow">
+        <div className="mb-8 border-t border-b border-black bg-zinc-200">
+          <Container>
+            <h2 className="py-2 text-5xl font-black text-center uppercase sm:text-6xl md:text-8xl xl:text-9xl">
+              Oops, something went wrong!
+            </h2>
+          </Container>
+        </div>
+        <Link
+          href="/"
+          className="block w-3/6 px-5 py-3 mx-auto text-center uppercase duration-300 border border-black bg-zinc-200 sm:text-xl lg:text-3xl shadow-strict focus:bg-accent hover:bg-accent transition-color"
+        >
+          Back to homepage
+        </Link>
+      </section>
+    </div>
+  );
+};
+
+export default ErrorPage;
